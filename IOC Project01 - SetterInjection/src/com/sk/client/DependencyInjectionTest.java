d9c398442f566639c5ec56acb31d2e665741b6dd
Suchit kumar <dhiman.suchit@gmail.com>
2025-02-22 13:43:31 +0530

    added season finder

--- IOC Project01 - SetterInjection/src/com/sk/client/DependencyInjectionTest.java
+++ IOC Project01 - SetterInjection/src/com/sk/client/DependencyInjectionTest.java
@@ -1,7 +1,11 @@
 package com.sk.client;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import com.sk.sbeans.SeasonFinder;
 import com.sk.sbeans.WishMessageGenrator;
 
 public class DependencyInjectionTest {
@@ -11,12 +15,21 @@ public class DependencyInjectionTest {
 
         //get target spring bean class obj from the IOC container
         Object obj = ctx.getBean("wmg");
+        Object obj1 = ctx.getBean("seas");
         // Get target bean
         WishMessageGenrator generator = (WishMessageGenrator)obj;
+        
+        SeasonFinder seasonFinder = (SeasonFinder)obj1;
 
         // Invoke the method
         String result = generator.generateWishMessage("Suchit");
         System.out.println("Result: " + result);
+        
+        //invoke season method
+        String currentSeason = seasonFinder.generateSeaon("kk");
+        System.out.println(currentSeason);
+        
+        
 
         // Close context (not needed for ClassPathXmlApplicationContext)
         ctx.close();
