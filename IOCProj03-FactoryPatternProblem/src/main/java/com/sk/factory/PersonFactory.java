7858e5be7ed32db8085987ef0c3f297bc54e851c
skdhi <skdhi@Suchit>
2026-04-14 22:45:05 +0530

    factory method problem

---
+++ IOCProj03-FactoryPatternProblem/src/main/java/com/sk/factory/PersonFactory.java
@@ -0,0 +1,16 @@
+package com.sk.factory;
+
+import com.sk.com.Employee;
+import com.sk.com.Person;
+
+public class PersonFactory {
+	
+	public static Person getIntance(String personType){
+		Person person = null;
+		
+		if(personType.equalsIgnoreCase("emp")) {
+			
+		}
+		return null;
+	}
+}
