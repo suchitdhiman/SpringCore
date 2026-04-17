0d291eec753a52b78e6fb0d24759ae3727d16e14
skdhi <skdhi@Suchit>
2026-04-17 08:17:18 +0530

    new code

--- IOCProj05-StrategyDP-FactoryDP-corejava/src/main/java/com/sk/factory/FlipkartFactory.java
+++ IOCProj05-StrategyDP-FactoryDP-corejava/src/main/java/com/sk/factory/FlipkartFactory.java
@@ -6,26 +6,26 @@ import com.sk.comps.DTDC;
 import com.sk.comps.Flipkart;
 
 public class FlipkartFactory {
-	
+
 	public static Flipkart getInstance(String courierType) {
-		
-		//create dependant class obj
+
+		// create dependant class obj
 		Courier courier = null;
-		if(courierType.equalsIgnoreCase("dtdc")) {
-			courier= new DTDC();
-		}else if(courierType.equalsIgnoreCase("blueDart")) {
+		if (courierType.equalsIgnoreCase("dtdc")) {
+			courier = new DTDC();
+		} else if (courierType.equalsIgnoreCase("blueDart")) {
 			courier = new BlueDart();
-		}else {
+		} else {
 			throw new IllegalArgumentException("invalid courier type");
 		}
-		
-		//create target calss obj
+
+		// create target calss obj
 		Flipkart flipkart = new Flipkart();
-		
-		//assign dep class obj to target
+
+		// assign dep class obj to target
 		flipkart.setCourier(courier);
 		return flipkart;
-		
+
 	}
 
 }
